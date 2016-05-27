77e636620b2b2e0f41ca48735ff0b6414f39b39b
mcfacedude <samuel0808@gmail.com>
2016-05-27 12:37:14 +0200

    removed tracking of routes and implemented poison reverse as described in the lab

--- RouterNode.java
+++ RouterNode.java
@@ -15,10 +15,7 @@ public class RouterNode {
     private GuiTextArea myGUI;
     private RouterSimulator sim;
     private int[] costs = new int[RouterSimulator.NUM_NODES];
-    private int[] route = new int[RouterSimulator.NUM_NODES];
-    private int[][] neighbourTable = new int[RouterSimulator.NUM_NODES][RouterSimulator.NUM_NODES];
-    private List<Integer> neighbourList = new ArrayList<Integer>();
-    private int[] neighbourCosts = new int[RouterSimulator.NUM_NODES];
+    private List<Integer> neighbours = new ArrayList<Integer>();
 
     //--------------------------------------------------
     public RouterNode(int ID, RouterSimulator sim, int[] costs) {
@@ -30,17 +27,13 @@ public class RouterNode {
 
         printDistanceTable(); //Initial distancetable for the route
 
-        for(int i = 0; i < RouterSimulator.NUM_NODES; i++) //sets default route to myID
-            route[i] = ID;
-
         // Loops through the number of routers, if the distance to a router is less than INFINTY and the ID
         // is not the same as this router, then it is neighbour and we send a update with our cost list to it
         for (int i = 0; i < costs.length; i++){
            if (costs[i] < RouterSimulator.INFINITY && i != myID){
                RouterPacket routerPacket = new RouterPacket(ID, i, costs);
                System.out.println("mincost to router "+i+ " = "+routerPacket.mincost[i]+" from router "+ myID);
-               neighbourList.add(i);
-               neighbourCosts[i] = costs[i]; //sets initial cost to neighbour
+               neighbours.add(i);
                sendUpdate(routerPacket);
            }
         }
@@ -48,57 +41,38 @@ public class RouterNode {
 
     //--------------------------------------------------
     public void recvUpdate(RouterPacket pkt) {
-        System.out.println("\nRecvUpdate for router "+ myID +" from router "+ pkt.sourceid);
+        System.out.println("\nRecrived Update for router "+ myID +" from router "+ pkt.sourceid);
 
-        neighbourTable[pkt.sourceid] = pkt.mincost; //updates the neigbourTable
-        boolean updated = false; //net yet updated any costs
+        for (int routerID = 0; routerID < pkt.mincost.length; routerID++) { //loops through all the costs
 
-        for (int routerID = 0; routerID < pkt.mincost.length; routerID++){ //loops through all the costs
+            if (pkt.mincost[routerID] != 0 && pkt.mincost[routerID] + costs[pkt.sourceid] < costs[routerID]){
 
-            //sourceRouters cost to a router + our cost to sourcerouter
-            int newCost = pkt.mincost[routerID] + costs[pkt.sourceid];
-
-            //Standard case
-            //if newCost < than our old cost to that router
-            if (newCost < costs[routerID]){
-                route[routerID] = pkt.sourceid;
-                costs[routerID] = newCost;
+                costs[routerID] = pkt.mincost[routerID]+ costs[pkt.sourceid];
                 System.out.println("Cost from router "+ myID +" to router "+ routerID +" is updated to "+ costs[routerID]);
-                updated = true;
+                updateNeighbours();
 
-             //if we are using sourceRouter for any current routs
-            }else if(pkt.sourceid == route[routerID]){
+            }
+        }
+    }
 
-                //The used router must be a neigbour, we only get updates from neighbours
-                for (int neighbour: neighbourList){
+    private void updateNeighbours(){
+        int[] tempCost = new int[costs.length];
+        System.arraycopy(costs, 0, tempCost, 0, costs.length);
+        RouterPacket routerPacket = null;
 
-                    //If the cost to a neighbour and then to a route < newCost
-                    if((neighbourCosts[neighbour] + neighbourTable[neighbour][routerID]) <= newCost){
+        for (int routerID = 0; routerID < costs.length; routerID++){
 
-                        //If the cost is not the same, we update
-                        if((neighbourCosts[neighbour] + neighbourTable[neighbour][routerID]) != newCost){
-                            updated = true;
-                        }
-
-                        newCost = neighbourCosts[neighbour] + neighbourTable[neighbour][routerID];
-
-                        System.out.println("Old path to "+ routerID +" through " + route[routerID] +
-                                " updated to path through " + neighbour +
-                                " with cost: " + newCost);
-
-                        route[routerID] = neighbour == routerID ? myID : neighbour;
-                        costs[routerID] = newCost;
-                    }
-                }
+            if (!neighbours.contains(routerID)){
+                tempCost[routerID] = RouterSimulator.INFINITY;
+                routerPacket = new RouterPacket(myID, routerID, tempCost);
+            }
+            else {
+                routerPacket = new RouterPacket(myID, routerID, costs);
             }
         }
 
-        if (updated){
-            for (int neighbour: neighbourList){
-
-                RouterPacket routerPacket = new RouterPacket(myID, neighbour, costs);
-                sendUpdate(routerPacket);
-            }
+        for (int neighbour: neighbours){
+            sendUpdate(routerPacket);
         }
     }
 
@@ -135,7 +109,7 @@ public class RouterNode {
     //--------------------------------------------------
     public void updateLinkCost(int dest, int newcost) {
         System.out.println("updateLinkCost for router "+ myID +" to router "+ dest + " with cost "+ newcost);
-
+        /*
         neighbourCosts[dest] = newcost;
         if (route[dest] == myID){
             costs[dest] = newcost;
@@ -147,6 +121,7 @@ public class RouterNode {
             sendUpdate(routerPacket);
         }
         printDistanceTable(); //Prints the updated distance table
+        */
     }
 
 }
